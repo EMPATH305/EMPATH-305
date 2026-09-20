55a07c006d02c596d2779bd27e872309275cf3c5
EMPATH305 <sheenafish0330@gmail.com>
2026-09-20 14:01:30 +0800

    Bump service worker cache to v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'empath-cache-v8'; // 安全渲染與深連結修正版
+const CACHE_NAME = 'empath-cache-v9'; // 安全渲染與深連結修正版
 
 self.addEventListener('install', (event) => {
     self.skipWaiting(); // 強制立即更新守護者
